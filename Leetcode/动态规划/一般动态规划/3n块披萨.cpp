cee0a94d7fa3c1aa09c1baa2b35d33a9762dd6a4
jaykay233 <zhiyuanx@usc.edu>
2020-10-17 08:42:04 +0800

    add

--- Leetcode/动态规划/一般动态规划/3n块披萨.cpp
+++ Leetcode/动态规划/一般动态规划/3n块披萨.cpp
@@ -23,3 +23,40 @@ public:
         return max(calculate(ans1),calculate(ans2));
     }
 };
+
+
+class Solution2{
+public:  
+    int maxSizeSlices(vector<int>& slices){
+        int n = slices.size();
+        vector<int> linkL(n);
+        vector<int> linkR(n);
+        for(int i =0;i<n;i++){
+            linkL[i] = (i==0?n-1:i-1);
+            linkR[i] = (i==n-1?0:i+1);
+        }
+
+        vector<int> valid(n,1);
+        priority_queue<pair<int,int>> q;
+        
+        for(int i=0;i<n;i++){
+            q.emplace(slices[i],i);
+        }
+
+        int ans =0 ;
+        for(int i=0;i<n/3;i++){
+            while(!valid[q.top().second]) q.pop();
+            int pos = q.top().second;
+            q.pop();
+            ans+=slices[pos];
+            slices[pos] = slices[linkL[pos]] + slices[linkR[pos]] - slices[pos];
+            q.emplace(slices[pos],pos);
+            valid[linkL[pos]] = valid[linkR[pos]] = 0;
+            linkR[linkL[linkL[pos]]] = pos;
+            linkL[linkR[linkR[pos]]] = pos;
+            linkL[pos] = linkL[linkL[pos]];
+            linkR[pos] = linkR[linkR[pos]];
+        }
+        return ans;
+    }
+};
