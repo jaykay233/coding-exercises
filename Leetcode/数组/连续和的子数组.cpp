d3f8091277546c9cd590e2713d3b6df63427c87e
xuzhiyuan <xuzhiyuan@bilibili.com>
2020-10-22 22:30:55 +0800

    add

--- Leetcode/数组/连续和的子数组.cpp
+++ Leetcode/数组/连续和的子数组.cpp
@@ -1,7 +1,10 @@
+#include <bits/stdc++.h>
+using namespace std;
+
 class Solution {
 public:
     bool checkSubarraySum(vector<int>& nums, int k) {
-        int sum  =0;
+        int sum =0;
         unordered_map<int,int> m;
         m[0] = -1;
         for(int i =0;i<nums.size();i++){
