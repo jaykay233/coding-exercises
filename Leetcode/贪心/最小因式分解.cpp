fa2cc91c65b6e4793f1d708f67e98bc8dc732158
jaykay233 <zhiyuanx@usc.edu>
2020-12-26 15:38:37 +0800

    add

---
+++ Leetcode/贪心/最小因式分解.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int smallestFactorization(int a) {
+        if(a<2)
+            return a;
+        long res =0, mul =1;
+        for(int i=9;i>=2;i--){
+            while(a%i==0){
+                a/=i;
+                res = mul * i + res;
+                mul *= 10;
+            }
+        }
+        return a<2 && res<INT_MAX ? (int)res:0;
+    }
+};
