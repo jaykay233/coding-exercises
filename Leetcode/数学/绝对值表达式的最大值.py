055c3d55f587ae2b47978c514cf1241b1d6f8d1f
jaykay233 <zhiyuanx@usc.edu>
2020-12-26 17:04:40 +0800

    add

---
+++ Leetcode/数学/绝对值表达式的最大值.py
@@ -0,0 +1,21 @@
+# 枚举8种可能性
+class Solution(object):
+    def maxAbsValExpr(self, arr1, arr2):
+        """
+        :type arr1: List[int]
+        :type arr2: List[int]
+        :rtype: int
+        """
+        A, B, C, D= [], [], [], []
+        for i in range(len(arr1)):
+            x, y = arr1[i], arr2[i]
+            A.append(x + y + i)
+            B.append(x + y - i)
+            C.append(x - y + i)
+            D.append(x - y - i)
+            
+        a = max(A) - min(A)
+        b = max(B) - min(B)
+        c = max(C) - min(C)
+        d = max(D) - min(D)        
+        return max(a, b, c, d)
