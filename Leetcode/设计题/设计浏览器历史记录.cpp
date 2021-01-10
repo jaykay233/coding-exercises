f1cf6d56a99cbc3c812803b74562c630dfa37246
jaykay233 <zhiyuanx@usc.edu>
2021-01-10 19:00:26 +0800

    add

---
+++ Leetcode/设计题/设计浏览器历史记录.cpp
@@ -0,0 +1,29 @@
+class BrowserHistory {
+public:
+    int pos;
+    int top;
+    string history[5001];
+    BrowserHistory(string homepage) : pos(-1), top(0) {
+        visit(homepage);
+    }
+    
+    void visit(string url) {
+        pos ++;
+        top = pos;
+        history[top++] = url;
+    }
+    
+    string back(int steps) {
+        if(steps > pos) {
+            steps = pos;
+        }
+        pos -= steps;
+        return history[pos];
+    }
+    
+    string forward(int steps) {
+        steps = min(steps, top - pos - 1);
+        pos += steps;
+        return history[pos];
+    }
+};
