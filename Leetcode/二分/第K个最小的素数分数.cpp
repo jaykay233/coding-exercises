40fa85eb2f39fc019326508d7fb79ea5c87953e7
jaykay233 <zhiyuanx@usc.edu>
2021-01-10 21:34:57 +0800

    add

---
+++ Leetcode/二分/第K个最小的素数分数.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+class Solution{
+public:
+    vector<int> kthSmallestPrimeFraction(vector<int>& A,int K){
+        double left = 0.0;
+        double right = 1.0;
+        int n =A.size();
+        while(left<right){
+            double mid = (right + left)/2.0;
+            int cnt =0 ;
+            vector<int> maxi = {0,1};
+            int j = 0;
+            for(int i =0;i<n;i++){
+                while(j<n && A[i]>=mid * A[j]) j++;
+                cnt += n-j;
+                if(j<n && maxi[0]*A[j]<A[i]*maxi[1]) maxi={A[i],A[j]};
+            }
+            if(cnt==K) return maxi;
+            if(cnt<K) left = mid;
+            else right = mid;
+        }
+        return {};
+    }
+};
