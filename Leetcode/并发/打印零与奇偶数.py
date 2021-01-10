3bd92a0fd950a350b74fc37737a7da2143ecc8dc
jaykay233 <zhiyuanx@usc.edu>
2021-01-10 10:19:41 +0800

    add

--- Leetcode/并发/打印零与奇偶数.py
+++ Leetcode/并发/打印零与奇偶数.py
@@ -1,3 +1,4 @@
+##三把锁
 import threading
 class ZeroEvenOdd(object):
     def __init__(self, n):
@@ -50,5 +51,46 @@ class ZeroEvenOdd(object):
                 self.oddLock.acquire()
                 printNumber(i)
                 self.zeroLock.release()
+
+
+##条件变量+一把锁+notify_all()
+from threading import Lock, Condition
+class ZeroEvenOdd:
+    def __init__(self, n):
+        self.n = n
+        self.num = 0
+        self.cv = Condition()
+        
+        
+	# printNumber(x) outputs "x", where x is an integer.
+    def zero(self, printNumber: 'Callable[[int], None]') -> None:
+        for i in range(self.n):
+            with self.cv:
+                while self.num % 2 != 0:
+                    self.cv.wait()
+                printNumber(0)
+                self.num += 1
+                self.cv.notify_all()
+        
+        
+    def even(self, printNumber: 'Callable[[int], None]') -> None:
+        for i in range(self.n - (self.n+1)//2):
+            with self.cv:
+                while self.num % 4 !=3:
+                    self.cv.wait()
+                printNumber((self.num+1)//2)
+                self.num += 1
+                self.cv.notify_all()    
+        
+        
+    def odd(self, printNumber: 'Callable[[int], None]') -> None:
+        for i in range((self.n+1)//2):
+            with self.cv:
+                while self.num % 4 != 1:
+                    self.cv.wait()
+                printNumber((self.num+1)//2)
+                self.num += 1
+                self.cv.notify_all()
+
         
         
