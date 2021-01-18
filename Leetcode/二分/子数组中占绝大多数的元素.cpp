48d390272b2d126b39dfc8fe2bdeb6034fcbd44d
jaykay233 <zhiyuanx@usc.edu>
2021-01-18 21:45:26 +0800

    add

--- Leetcode/二分/子数组中占绝大多数的元素.cpp
+++ Leetcode/二分/子数组中占绝大多数的元素.cpp
@@ -1,3 +1,4 @@
+//超时
 #include <bits/stdc++.h>
 using namespace std;
 //https://leetcode-cn.com/problems/online-majority-element-in-subarray/solution/shu-tao-shu-by-elvis-10/
@@ -103,3 +104,77 @@ public:
  * MajorityChecker* obj = new MajorityChecker(arr);
  * int param_1 = obj->query(left,right,threshold);
  */
+
+
+//换种写法的树套树
+const int MAXN=2e4+50;
+const int MAXM=2e4;
+
+struct BTree{ int ch[2], sum; }node[MAXN*100];
+int root[MAXN], numn, PL, PR;
+
+inline int lowbit(int x){ return x&-x; }
+
+void init(){ 
+    numn=0;
+    memset(root, 0, sizeof(root));
+}
+
+void insertNode(int &x, int l, int r, int p){
+    if (!x) { x=++numn; node[x].ch[0]=node[x].ch[1]=node[x].sum=0; }
+
+    ++node[x].sum;
+
+    if (l<r){
+        int m=(l+r)/2;
+        if (p<=m){
+            insertNode(node[x].ch[0], l, m, p);
+        }else insertNode(node[x].ch[1], m+1, r, p);
+    }
+}
+
+void insertTree(int p, int v){
+    for (int i=v; i<=MAXM; i+=lowbit(i)) 
+        insertNode(root[i], PL, PR, p);
+}
+
+int queryNode(int x, int l, int r, int left, int right){
+    if (!x) return 0;
+    if (left<=l && r<=right) return node[x].sum;
+
+    int m=(l+r)/2, ret=0;
+    if (left<=m) ret+=queryNode(node[x].ch[0], l, m, left, right);
+    if (m+1<=right) ret+=queryNode(node[x].ch[1], m+1, r, left, right);
+
+    return ret;
+}
+
+int queryTree(int left, int right, int v){
+    int ret=0;
+    for (int i=v; i>0; i-=lowbit(i))
+        ret+=queryNode(root[i], PL, PR, left, right);
+    return ret;
+}
+
+class MajorityChecker {
+public:
+    MajorityChecker(vector<int>& a) {
+        PL=0, PR=a.size()-1;
+        init();
+        for (int i=PL; i<=PR; i++) insertTree(i, a[i]);
+    }
+    
+    int query(int left, int right, int threshold) {
+        int ans=0, now=32768/2, k=threshold;
+        while(now){
+            if (now+ans>MAXM) { now/=2; continue; }
+            int v=queryNode(root[ans+now], PL, PR, left, right);
+            if (v<k) { k-=v; ans+=now; }
+            now/=2;
+        }
+        
+        int cnt=queryTree(left, right, ans+1) - queryTree(left, right, ans);
+        return cnt>=threshold?ans+1:-1;
+    }
+};
+
