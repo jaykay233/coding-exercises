2af6b595d4a3bac4abe35c23864f49195f1539b0
jaykay233 <zhiyuanx@usc.edu>
2021-03-27 15:18:04 +0800

    add

--- 算法竞赛入门进阶/DP/数位DP/hdu3652.cpp
+++ 算法竞赛入门进阶/DP/数位DP/hdu3652.cpp
@@ -1,5 +1,20 @@
 #include <bits/stdc++.h>
 using namespace std;
+typedef long long ll;
+const int MOD = 13;
+ll dp[20][3][13];
+int dis[20];
+
+ll dfs(int len,int type,int mod,bool flag){
+    if(len<0) return type==2 && mod ==0;
+    if(!flag && dp[len][type][mod]!=-1) return dp[len][type][mod];
+    int end = flag?dis[len]:9;
+    int ans = 0;
+    for(int i=0;i<=end;i++){
+        if(type==2 || (type==1 && i==3))
+            ans += dfs(len-1,2,(mod*10+i)%MOD, flag && i==end);
+    }
+}
 
 
 int main(){
