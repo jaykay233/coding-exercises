c0685513a31eaed9bdd61487dc2f95ebd1b2b4dc
jaykay233 <zhiyuanx@usc.edu>
2021-03-27 17:06:12 +0800

    add

--- 算法竞赛入门进阶/DP/数位DP/hdu3652.cpp
+++ 算法竞赛入门进阶/DP/数位DP/hdu3652.cpp
@@ -13,12 +13,29 @@ ll dfs(int len,int type,int mod,bool flag){
     for(int i=0;i<=end;i++){
         if(type==2 || (type==1 && i==3))
             ans += dfs(len-1,2,(mod*10+i)%MOD, flag && i==end);
+        else
+            ans += dfs(len-1,i==1?1:0,(mod*10+i)%MOD,flag && i==end);
     }
+    if(!flag)
+        dp[len][type][mod] = ans;
+    return ans;
 }
 
 
+ll solve(ll n){
+    int len =0 ;
+    while(n){
+        dis[len++] = n%10;
+        n/=10;
+    }
+    return dfs(len-1,0,0,true);
+}
+
 int main(){
-
-
+    int n;
+    memset(dp,-1,sizeof dp);
+    while(cin>>n)
+        cout<<solve(n)<<endl;
+    
     return 0;
 }
