c742c9f892c6e9c1aa249ab9b19432e727ad1f8a
jaykay233 <zhiyuanx@usc.edu>
2021-05-15 20:43:00 +0800

    add all

---
+++ Leetcode/数学/n个骰子的点数.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+class Solution{
+public:  
+    const int maxn = 12;
+    vector<double> dicesProbability(int n){
+        int dp[15][70];
+        memset(dp,0,sizeof dp);
+        for(int i=1;i<=6;i++){
+            dp[1][i] = 1;
+        }
+        for(int i=2;i<=n;i++){
+            for(int j =i;j<=6*i;j++){
+                for(int cur=1;cur<=6;cur++){
+                    if(j-cur<=0)
+                        break;
+                    dp[i][j] += dp[i-1][j-cur];
+                }
+            }
+        }
+        int all = pow(6,n);
+        vector<double> ret;
+        for(int i=1;i<=6*n;i++){
+            ret.push_back(dp[n][i]*1.0/all);
+        }
+        return ret;
+    }
+};
---
+++ Leetcode/数学/不用加减乘除做加法2.cpp
@@ -0,0 +1,7 @@
+#include <bits/stdc++.h>
+class Solution{
+public:  
+    int add(int a, int b){
+        return b==0?a:add(a^b,(unsigned int)(a&b)<<1);
+    }
+};
