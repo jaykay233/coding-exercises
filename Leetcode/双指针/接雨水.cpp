769663238a6c6db145c89653f1f6bcf95a2abddd
jaykay233 <zhiyuanx@usc.edu>
2021-05-16 16:01:32 +0800

    add all

---
+++ Leetcode/双指针/接雨水.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int trap(vector<int>& height) {
+        int l = 0, r = height.size()-1;
+        int ans = 0;
+        int leftmax = 0, rightmax =0 ;
+        while(l<r){
+            leftmax = max(leftmax,height[l]);
+            rightmax = max(rightmax,height[r]);
+            if(leftmax < rightmax){
+                ans += leftmax - height[l];
+                l++;
+            }else{
+                ans += rightmax - height[r];
+                r--;
+            }
+        }
+        return ans;
+    }
+};
