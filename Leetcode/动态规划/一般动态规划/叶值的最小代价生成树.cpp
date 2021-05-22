02b4de4bddb4b88b34120d039c09c26a4aba9eae
jaykay233 <zhiyuanx@usc.edu>
2021-05-22 20:32:16 +0800

    add all

---
+++ Leetcode/动态规划/一般动态规划/叶值的最小代价生成树.cpp
@@ -0,0 +1,24 @@
+//https://leetcode-cn.com/problems/minimum-cost-tree-from-leaf-values/
+#include <bits/stdc++.h>
+using namespace std;
+class Solution{
+public:  
+    int mctFromLeafValues(vector<int>& arr){
+        stack<int> st;
+        st.push(INT_MAX);
+        int res =0 ;
+        for(int i=0;i<arr.size();i++){
+            while(arr[i] >= st.top()){
+                int t = st.top();
+                st.pop();
+                res += t* min(st.top(),arr[i]);
+            }
+            st.push(arr[i]);
+        }
+        while(arr.size()>2){
+            int t = st.top();st.pop();
+            res += t * st.top();
+        }
+        return res;
+    }
+};
