5cff690dc3484830821fa0332ac8930fac2191eb
saboxu <saboxu@tencent.com>
2021-07-10 16:55:52 +0800

    add new file

---
+++ Leetcode/动态规划/除数博弈.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    bool divisorGame(int n) {
+        vector<int> f(n+5,false);
+        f[1] = false;
+        f[2] = true;
+        for(int i=3;i<=n;i++){
+            for(int j=1;j<i;j++){
+                if(i%j==0 && !f[i-j]){
+                    f[i] = true;
+                    break;
+                }
+            }
+        }
+        return f[n];
+    }
+};
