87eaa2897d981063228f7a00479d09b033ef496b
saboxu <saboxu@tencent.com>
2021-07-11 18:02:47 +0800

    add new file

---
+++ Leetcode/贪心/有效的括号字符串.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+class Solution {
+public:
+    bool checkValidString(string s) {
+        int high = 0;
+        int low = 0;
+        for(int i =0;i<s.size();i++){
+            if(s[i]=='(') {
+                high++;
+                low++;
+            }else if(s[i]==')'){
+                high--;
+                low--;
+            }else if(s[i]=='*'){
+                high++;
+                low--;
+            }
+            if(high<0) return false;
+            low = max(low,0);
+        }
+        return low == 0;
+    }
+};
