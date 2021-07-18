46a844d0097d948f654c67b5479b862974397fcd
saboxu <saboxu@tencent.com>
2021-07-18 15:56:29 +0800

    add all

---
+++ Leetcode/数据结构/最长有效括号.cpp
@@ -0,0 +1,47 @@
+#include <bits/stdc++.h>
+using namespace std;
+class Solution{
+public:  
+    int longestValidPalindrome(string s){
+        stack<int> st;
+        int maxlen = 0;
+        vector<int> dp(s.size(),0);
+        for(int i =0;i<s.size();i++){
+            if(s[i]=='(')
+                st.push(i);
+            else if(s[i] == ')'){
+                if(!st.empty()){
+                    dp[i] = (st.top()>1=1?dp[st.top()-1]:0) + i - st.top() + 1;
+                    maxlen = max(maxlen, dp[i]);
+                    st.pop();
+                }else{
+                    continue;
+                }
+            }
+        }
+        return maxlen;
+    }
+};
+
+//https://leetcode-cn.com/problems/longest-valid-parentheses/solution/zui-chang-you-xiao-gua-hao-by-leetcode-solution/
+class Solution2{
+public:    
+    int longestValidParentheses(string s){
+        int maxans = 0;
+        stack<int> stk;
+        stk.push(-1);
+        for(int i =0;i<s.size();i++){
+            if(s[i]=='(')
+                stk.push(i);
+            else{
+                stk.pop();
+                if(stk.empty())
+                    stk.push(i);
+                else{
+                    maxans = max(maxans, i - stk.top());
+                }
+            }
+        }
+        return maxans;
+    }
+}
