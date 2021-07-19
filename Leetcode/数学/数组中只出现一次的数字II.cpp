7cbf666470a4cc6813b8f7f9b4a7035b72fb6100
saboxu <saboxu@tencent.com>
2021-07-20 00:05:07 +0800

    add all

---
+++ Leetcode/数学/数组中只出现一次的数字II.cpp
@@ -0,0 +1,13 @@
+//https://leetcode-cn.com/problems/shu-zu-zhong-shu-zi-chu-xian-de-ci-shu-ii-lcof/solution/shu-ju-jie-gou-he-suan-fa-san-chong-jie-4b9se/
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+        int one = 0, two=0;
+        for(int i=0;i<nums.size();i++){
+            int n = nums[i];
+            one = (one & (~two) & (~n)) | n & (~two) & (~one);
+            two = (~n & two & ~one) | (n & ~two & ~one);
+        }
+        return one;
+    }
+};
