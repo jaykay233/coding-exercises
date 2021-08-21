433098b51711f9fa0beb63690cc9c4eeab35258b
jaykay233 <saboxu@tencent.com>
2021-08-21 15:21:57 +0800

    add all

---
+++ Leetcode/二分/排序数组中只出现一次的数字.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int singleNonDuplicate(vector<int>& nums) {
+        int n = nums.size();
+        int l = 0;
+        int r = n -1;
+        while(l<r){
+            int mid = l + r >>1;
+            if(mid & 1)
+                mid -= 1;
+            if(nums[mid] == nums[mid + 1])
+                l = mid + 2;
+            else 
+                r = mid;
+        }
+        return nums[l];
+    }
+};
