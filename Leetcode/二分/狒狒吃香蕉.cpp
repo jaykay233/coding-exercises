2ada23b8e7cac8a6b1c92f9b91b813e360f73487
jaykay233 <saboxu@tencent.com>
2021-08-21 14:47:02 +0800

    add all

---
+++ Leetcode/二分/狒狒吃香蕉.cpp
@@ -0,0 +1,26 @@
+class Solution 
+{
+public:
+    int minEatingSpeed(vector<int>& piles, int h) 
+    {
+        int l = 1;
+        int r = *max_element(piles.begin(), piles.end());
+        while (l < r)
+        {
+            int mid = l + (r - l) / 2;
+            if (check(mid, piles, h) == true)
+                r = mid;
+            else
+                l = mid + 1;
+        }
+        return l;
+    }
+
+    bool check(int speed, vector<int> & piles, int h)
+    {
+        int cnt = 0;
+        for (int p : piles)
+            cnt += ceil((double)p / speed);
+        return cnt <= h;
+    }
+};
