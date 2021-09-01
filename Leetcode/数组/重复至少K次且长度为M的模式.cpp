bf44ba682a19c2ecedeeb0c0ca044868e72eb632
saboxu <saboxu@tencent.com>
2021-09-01 13:56:20 +0800

    add all

---
+++ Leetcode/数组/重复至少K次且长度为M的模式.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    bool containsPattern(vector<int>& arr, int m, int k) {
+        int n = arr.size();
+        for (int l = 0; l <= n - m * k; ++l) {
+            int offset;
+            for (offset = 0; offset < m * k; ++offset) {
+                if (arr[l + offset] != arr[l + offset % m]) {
+                    break;
+                }
+            }
+            if (offset == m * k) {
+                return true;
+            }
+        }
+        return false;
+    }
+};
