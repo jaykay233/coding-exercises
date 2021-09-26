1c6b040ed843decec15843b2c2ea1c2171323902
jaykay233 <saboxu@tencent.com>
2021-09-26 23:11:20 +0800

    add all

--- apue/abcd.c
+++ apue/abcd.c
@@ -4,11 +4,23 @@
 #include <string.h>
 #include <signal.h>
 #define THRUM   4
+static pthread_mutex_t mut[THRUM];
+
+static next(int n){
+    if(n + 1 == THRUM)
+        return 0;
+    return n + 1;
+}
+
 
 static void *thr_func(void* p){
-    int c = 'a' + (int)p;
-    while(1)
+    int n = (int)p;
+    int c = 'a' + n;
+    while(1){
+        pthread_mutex_lock(mut+n);
         write(1,&c,1);
+        pthread_mutex_unlock(mut+next(n));
+    }
     pthread_exit(NULL);
 }
 
@@ -17,12 +29,18 @@ int main(){
     int i;
     pthread_t tid[THRUM];
     for(i =0;i<THRUM;i++){
+        pthread_mutex_init(mut+i, NULL);
+        pthread_mutex_lock(mut+i);
+
         err = pthread_create(tid+i,NULL,thr_func, (void*)i);
         if(err){
             fprintf(stderr,"pthread_creete():%s\n",strerror(err));
             exit(1);
         }
     }
+
+    pthread_mutex_unlock(mut+0);
+    
 
     alarm(5);
 
