436ecac98e2189d7a8702fcf201553813250d639
zhiyuan.xu <zhiyuan.xu@C02FXLNZMD6M.local>
2022-03-12 12:09:30 +0800

    add all

---
+++ Leetcode/动态规划/目标和.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int findTargetSumWays(vector<int>& nums, int target) {
+        int sum = 0;
+        for (int& num : nums) {
+            sum += num;
+        }
+        int diff = sum - target;
+        if (diff < 0 || diff % 2 != 0) {
+            return 0;
+        }
+        int neg = diff / 2;
+        vector<int> dp(neg + 1);
+        dp[0] = 1;
+        for (int& num : nums) {
+            for (int j = neg; j >= num; j--) {
+                dp[j] += dp[j - num];
+            }
+        }
+        return dp[neg];
+    }
+};
