454e3e1d54f0a9e4affcde0db8625ad58b5b8150
zhiyuan.xu <zhiyuan.xu@C02FXLNZMD6M.local>
2022-03-21 15:54:08 +0800

    add all

---
+++ Leetcode/数学/快乐数.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    bool isHappy(int n) {
+        int slow = n, fast = n;
+        do{
+            slow = bitSquareSum(slow);
+            fast = bitSquareSum(fast);
+            fast = bitSquareSum(fast);
+        }while(slow != fast);
+        
+        return slow == 1;
+    }
+    
+    int bitSquareSum(int n){
+        vector<int> digits;
+        while(n){
+            digits.push_back(n%10);
+            n/=10;
+        }
+        int sum = 0;
+        for(int d:digits){
+            sum += d*d;
+        }
+        return sum;
+    }
+};
